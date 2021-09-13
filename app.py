1a162d0b81afc3b2b9e8f0f4c0f42a48e03063b8
sirinehaddouche <60438964+sirinehaddouche@users.noreply.github.com>
2021-09-13 16:05:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,12 @@ output = query({"inputs" : text})
 
 if submit_button:
     st.subheader('Data')
-    st.write({"negative": output[0][0]['score']})
-    st.write({"positive": output[0][1]['score']})
+    
+    if type(output) == type({}):        
+        st.write({"Error": 'The model is loading, please try again in 30 seconds.'})
+    else:
+        st.write({"NEGATIVE": output[0][0]['score']})
+        st.write({"POSITIVE": output[0][1]['score']})
 
 
 
